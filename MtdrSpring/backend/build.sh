bf7d0adfdaa820db252459379bd2d5bb84b28e3e
BlitzExp <jsnajera1017@mail.com>
2026-04-17 02:24:05 -0600

    rechanged build.sh

--- MtdrSpring/backend/build.sh
+++ MtdrSpring/backend/build.sh
@@ -7,21 +7,18 @@ if [ -z "$DOCKER_REGISTRY" ]; then
     export DOCKER_REGISTRY=$(state_get DOCKER_REGISTRY)
     echo "DOCKER_REGISTRY set."
 fi
+
 if [ -z "$DOCKER_REGISTRY" ]; then
     echo "Error: DOCKER_REGISTRY env variable needs to be set!"
     exit 1
 fi
 
-export IMAGE=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
-export OCIR_HOST=$(echo "$DOCKER_REGISTRY" | cut -d'/' -f1)
+export IMAGE="${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}"
 
 mvn clean package spring-boot:repackage
-docker build -f Dockerfile -t $IMAGE .
+docker build -f Dockerfile -t "$IMAGE" .
+docker push "$IMAGE"
 
-echo "Login to OCIR host: $OCIR_HOST"
-docker login "$OCIR_HOST"
-
-docker push $IMAGE
 if [ $? -eq 0 ]; then
-    docker rmi "$IMAGE"
+    docker rmi "$IMAGE" # local
 fi

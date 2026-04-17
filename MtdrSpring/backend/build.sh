057746eeabab0423d6313c75f3a981dd1bad80cf
BlitzExp <jsnajera1017@mail.com>
2026-04-17 02:15:29 -0600

    changed build.sh

--- MtdrSpring/backend/build.sh
+++ MtdrSpring/backend/build.sh
@@ -3,7 +3,6 @@
 export IMAGE_NAME=todolistapp-springboot
 export IMAGE_VERSION=0.1
 
-
 if [ -z "$DOCKER_REGISTRY" ]; then
     export DOCKER_REGISTRY=$(state_get DOCKER_REGISTRY)
     echo "DOCKER_REGISTRY set."
@@ -14,11 +13,15 @@ if [ -z "$DOCKER_REGISTRY" ]; then
 fi
 
 export IMAGE=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
+export OCIR_HOST=$(echo "$DOCKER_REGISTRY" | cut -d'/' -f1)
 
 mvn clean package spring-boot:repackage
 docker build -f Dockerfile -t $IMAGE .
 
+echo "Login to OCIR host: $OCIR_HOST"
+docker login "$OCIR_HOST"
+
 docker push $IMAGE
-if [  $? -eq 0 ]; then
-    docker rmi "$IMAGE" #local
+if [ $? -eq 0 ]; then
+    docker rmi "$IMAGE"
 fi
